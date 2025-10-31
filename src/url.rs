7176443aafc28d338da76fe90fe99f22a73104bb
Nishanth R <nishusheela2011@gmail.com>
2025-10-31 12:01:36 -0400

    init rust

---
+++ src/url.rs
@@ -0,0 +1,13 @@
+use std::net;
+
+pub struct URL {
+    scheme: String, 
+    host: String, 
+    path: String,
+}
+
+// impl URL {
+//     fn new(&self, scheme: &str, host: &str, path: &str) -> Self {
+//        self.scheme = 
+// };
+

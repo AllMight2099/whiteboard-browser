249a370bcf9797e8499a59a8e79c819976915f17
Nishanth R <nishusheela2011@gmail.com>
2025-11-08 02:50:38 -0500

    adds url structs and constructor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod url;
+
+use url::URL;
+
 fn main() {
+    let url: URL = URL::url("https://example.com/path/example/yes".to_string()).unwrap();
+
     println!("Hello, world!");
+    println!("url as a whole: {:#?}", url);
 }
--- src/url.rs
+++ src/url.rs
@@ -1,10 +1,58 @@
-use std::net;
+use std::os::unix::net::{UnixListener, UnixStream};
 
+// TODO change this to &str later
+#[derive(Debug)]
 pub struct URL {
     scheme: String, 
     host: String, 
     path: String,
 }
+
+impl URL{
+    pub fn url(url: String) -> Option<Self> {
+        let scheme: String;
+        let host: String;
+        let path: String;
+
+        let url_str = url.as_str();
+
+        // let Some((sch, rem_url)) = url.split_once("://");
+        if let Some((sch, rem_url)) = url_str.split_once("://") {
+            println!("Scheme part: {}", rem_url);
+            scheme = sch.to_string();
+            if !rem_url.contains("/") {
+                host = rem_url.to_string();
+                path = "/".to_string();
+            } else {
+                let parts: Vec<&str> = rem_url.splitn(2, "/").collect();
+                host = parts[0].to_string();
+                path = format!("/{}", parts[1]);
+            }
+
+        } else {
+            // TODO: Handle error
+            return None;
+        }
+
+        println!("Scheme: {}", scheme);
+        
+        Some(Self {
+            scheme,
+            host,
+            path,
+        })
+    }
+
+
+    // pub fn request() {
+    //     socket = UnixStream::connect()
+    //     return;
+    // }
+
+}
+
+
+
 
 // impl URL {
 //     fn new(&self, scheme: &str, host: &str, path: &str) -> Self {

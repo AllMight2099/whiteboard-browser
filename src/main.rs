3d97a8f4b6769c68380c0bef34f5bd73fde404f3
Nishanth R <nishusheela2011@gmail.com>
2025-11-22 16:45:53 -0500

    parses html body without tags

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,31 @@ mod url;
 use url::URL;
 
 fn main() {
-    let url: URL = URL::url("https://example.com/path/example/yes".to_string()).unwrap();
+    let example_url = "https://browser.engineering/examples/xiyouji.html";
+    let url: URL = URL::url("https://example.com".to_string()).unwrap();
 
-    println!("Hello, world!");
-    println!("url as a whole: {:#?}", url);
+    load(url);
+
+    // println!("Hello, world!");
+
+    // println!("url as a whole: {:#?}", url);
+}
+
+fn load(url: URL) {
+    let content: String = url.request();
+    show(content)
+}
+
+fn show(body: String) {
+    println!();
+    let mut in_tag = false;
+    for c in body.chars() {
+        if c == '<' {
+            in_tag = true;
+        } else if c == '>'{
+            in_tag = false;
+        } else if !in_tag {
+            print!("{}", c);
+        }
+    }
 }
--- src/url.rs
+++ src/url.rs
@@ -1,53 +1,101 @@
 use std::os::unix::net::{UnixListener, UnixStream};
+use http::HeaderMap;
+use reqwest::Url;
+// use url::Url;
 
 // TODO change this to &str later
 #[derive(Debug)]
 pub struct URL {
-    scheme: String, 
-    host: String, 
-    path: String,
+    // scheme: String, 
+    // host: String, 
+    // path: String,
+    // uri: String,
+    url: Url
 }
 
 impl URL{
     pub fn url(url: String) -> Option<Self> {
-        let scheme: String;
-        let host: String;
-        let path: String;
+        // let scheme: String;
+        // let host: String;
+        // let path: String;
+        let uri: String;
 
-        let url_str = url.as_str();
+        let parsed = match Url::parse(&url) { 
+            Ok (a) => {
+                return Some(Self{url: a})
+            }
+            Err (_) => {
+                return None
+            }
+        };
+
+        // let url_str = url.as_str();
+        // uri = url;
 
         // let Some((sch, rem_url)) = url.split_once("://");
-        if let Some((sch, rem_url)) = url_str.split_once("://") {
-            println!("Scheme part: {}", rem_url);
-            scheme = sch.to_string();
-            if !rem_url.contains("/") {
-                host = rem_url.to_string();
-                path = "/".to_string();
-            } else {
-                let parts: Vec<&str> = rem_url.splitn(2, "/").collect();
-                host = parts[0].to_string();
-                path = format!("/{}", parts[1]);
-            }
+        // if let Some((sch, rem_url)) = url_str.split_once("://") {
+        //     println!("Scheme part: {}", rem_url);
+        //     scheme = sch.to_string();
+        //     if !rem_url.contains("/") {
+        //         host = rem_url.to_string();
+        //         path = "/".to_string();
+        //     } else {
+        //         let parts: Vec<&str> = rem_url.splitn(2, "/").collect();
+        //         host = parts[0].to_string();
+        //         path = format!("/{}", parts[1]);
+        //     }
 
-        } else {
-            // TODO: Handle error
-            return None;
-        }
+        // } else {
+        //     // TODO: Handle error
+        //     return None;
+        // }
 
-        println!("Scheme: {}", scheme);
+        // println!("Scheme: {}", scheme);
         
-        Some(Self {
-            scheme,
-            host,
-            path,
-        })
+        // Some(Self {
+        //     // scheme,
+        //     // host,
+        //     // path,
+        //     uri
+        // })
     }
 
 
-    // pub fn request() {
-    //     socket = UnixStream::connect()
-    //     return;
-    // }
+    pub fn request(self) -> String {
+        let request = reqwest::blocking::get(self.url.as_str());
+        let mut headers: HeaderMap = HeaderMap::new();
+        let mut body: String = String::new();
+        match request {
+            Ok(response) => {
+                // println!("Response Status: {}", response.status());
+                headers = response.headers().clone();
+
+                // println!("Response Headers:\n{:#?}", response.headers());
+                match response.text() {
+                    Ok(text) => {
+                        body = text;
+                    },
+                    Err(e) => {
+                        println!("Failed to read response body: {}", e);
+                    }
+                }
+                // println!("Response Body:\n{}", body);
+            }, 
+            Err(e) => {
+                println!("Request failed: {}", e);
+            }
+        }
+
+
+        for header in headers.iter() {
+            assert!(header.0 != "transfer-encoding");
+            assert!(header.0 != "content-encoding");
+            // println!("{}: {:?}", header.0, header.1);
+        }
+
+        // socket = UnixStream::connect()
+        return body;
+    }
 
 }
 
